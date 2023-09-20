cf151893ca942f1f160d2bbb3b3e03c72eb249ba
Mark Philpot <mark.philpot@gmail.com>
2023-09-19 21:07:30 -0700

    Initial functionality

---
+++ netlify/functions/trim.ts
@@ -0,0 +1,27 @@
+import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import fetch from "node-fetch";
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  const feed = event.queryStringParameters?.feed;
+
+  if (feed) {
+    const response = await fetch(feed);
+    const statusCode = response.status;
+    const content = await response.text();
+
+    return {
+      statusCode,
+      body: content.trim(),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: "Missing feed parameter",
+    };
+  }
+};
+
+export { handler };
